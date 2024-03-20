48b9509282566433adc7a2c8acfb03bc405ebe89
Joao Zanutto <jpberno@gmail.com>
2024-03-19 18:41:09 -0700

    unalign parameter (#2)
    * unalign parameter
    
    * re-run ci
    
    * Apply automatic changes
    
    * try adding name to ci file
    
    * fix workflow order
    
    * add required providers block
    
    * terraform-docs: automated action
    
    * add required terraform version
    
    * add gitignore file
    
    * terraform-docs: automated action
    
    * fix workflow order
    
    * remove whitespace
    
    * run ci with pinned tf version
    
    * add description for security group
    
    * ensure metadata service version 1 is not enabled
    
    * enable container insights
    
    * add description for security group rules
    
    ---------
    
    Co-authored-by: joao-zanutto <joao-zanutto@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- security_group.tf
+++ security_group.tf
@@ -1,12 +1,14 @@
 resource "aws_security_group" "ecs_node_sg" {
   name_prefix = "${local.identifier}-ecs-node-sg"
+  description = "Security Group for nodes in the ${local.identifier} ECS Cluster"
   vpc_id      = local.vpc_id
 
   ingress {
-    from_port = 0
-    to_port   = 65535
-    protocol  = "tcp"
-    self      = true
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    self        = true
+    description = "Enable all traffic on self"
   }
 
   egress {
@@ -14,5 +16,6 @@ resource "aws_security_group" "ecs_node_sg" {
     to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Enable all outbound traffic"
   }
 }

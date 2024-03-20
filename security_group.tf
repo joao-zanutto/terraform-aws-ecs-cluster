f1be608082f1e8223299e7432e03f7aa3dd741c7
joao-zanutto <jpberno@gmail.com>
2024-03-19 18:39:18 -0700

    add description for security group rules

--- security_group.tf
+++ security_group.tf
@@ -4,10 +4,11 @@ resource "aws_security_group" "ecs_node_sg" {
   vpc_id      = local.vpc_id
 
   ingress {
-    from_port = 0
-    to_port   = 65535
-    protocol  = "tcp"
-    self      = true
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    self        = true
+    description = "Enable all traffic on self"
   }
 
   egress {
@@ -15,5 +16,6 @@ resource "aws_security_group" "ecs_node_sg" {
     to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Enable all outbound traffic"
   }
 }

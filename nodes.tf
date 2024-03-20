3e4354521d9c56a63588b447a88ea3be9901f011
joao-zanutto <jpberno@gmail.com>
2024-03-19 18:32:09 -0700

    ensure metadata service version 1 is not enabled

--- nodes.tf
+++ nodes.tf
@@ -7,6 +7,8 @@ resource "aws_launch_template" "ecs_ec2" {
   iam_instance_profile { arn = aws_iam_instance_profile.ecs_node.arn }
   monitoring { enabled = true }
 
+  metadata_options { http_tokens = "required" }
+
   user_data = base64encode(<<-EOF
       #!/bin/bash
       echo ECS_CLUSTER=${aws_ecs_cluster.main.name} >> /etc/ecs/ecs.config;
